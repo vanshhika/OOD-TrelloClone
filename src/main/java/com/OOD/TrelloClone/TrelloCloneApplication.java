f07dc009ab9c5aa4d74f6ba93453b1fe116fc0bc
vanshhika <vinnypatel2000@gmail.com>
2023-10-25 23:19:41 -0400

    Initial Commit

---
+++ src/main/java/com/OOD/TrelloClone/TrelloCloneApplication.java
@@ -0,0 +1,13 @@
+package com.OOD.TrelloClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OOD/TrelloClone/TrelloCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OOD.TrelloClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
