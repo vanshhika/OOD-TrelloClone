784278c4c595d6cb1a34af7099ecbe8b80cad4f5
Mohit Kambli <mohitkambli8@gmail.com>
2023-10-30 22:56:44 -0400

    Added interface that extends JPA Repository

---
+++ src/main/java/com/OOD/TrelloClone/repository/TaskEntityRepository.java
@@ -0,0 +1,10 @@
+package com.OOD.TrelloClone.repository;
+
+import com.OOD.TrelloClone.model.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskEntityRepository extends JpaRepository<TaskEntity, Long> {
+
+}
