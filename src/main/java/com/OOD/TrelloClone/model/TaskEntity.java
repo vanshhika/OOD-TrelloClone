1aed3dc0ad331c17032779a35ac7e635ab4e4e9c
Mohit Kambli <mohitkambli8@gmail.com>
2023-10-30 22:52:04 -0400

    Updated task related properties

--- src/main/java/com/OOD/TrelloClone/model/TaskEntity.java
+++ src/main/java/com/OOD/TrelloClone/model/TaskEntity.java
@@ -1,16 +1,90 @@
 package com.OOD.TrelloClone.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.*;
-import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskEntity {
+    public long getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(long taskID) {
+        this.taskID = taskID;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTodoTime() {
+        return todoTime;
+    }
+
+    public void setTodoTime(String todoTime) {
+        this.todoTime = todoTime;
+    }
+
+    public String getDoneTime() {
+        return doneTime;
+    }
+
+    public void setDoneTime(String doneTime) {
+        this.doneTime = doneTime;
+    }
+
+    public String getDoingTime() {
+        return doingTime;
+    }
+
+    public void setDoingTime(String doingTime) {
+        this.doingTime = doingTime;
+    }
+
+    public TaskState getState() {
+        return state;
+    }
+
+    public void setState(TaskState state) {
+        this.state = state;
+    }
+
+    public UserEntity getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(UserEntity assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Exclude
@@ -21,4 +95,11 @@ public class TaskEntity {
     private String todoTime;
     private String doneTime;
     private String doingTime;
+    @Enumerated(EnumType.STRING)
+    private TaskState state;
+    @ManyToOne
+    private UserEntity assignedTo;
+    @ElementCollection
+    private List<String> comments;
+
 }
