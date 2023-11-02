e0695dfc3c627406f367b56a9f7f1a0db3f1c874
“sayali1998” <sayalipatil631@gmail.com>
2023-11-01 23:42:26 -0400

    Minor

--- src/main/java/com/OOD/TrelloClone/repository/TaskUserEntityRepository.java
+++ src/main/java/com/OOD/TrelloClone/repository/TaskUserEntityRepository.java
@@ -3,11 +3,11 @@ package com.OOD.TrelloClone.repository;
 import com.OOD.TrelloClone.model.TaskEntity;
 import com.OOD.TrelloClone.model.TaskUsersEntity;
 import com.OOD.TrelloClone.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface TaskUserEntityRepository {
+public interface TaskUserEntityRepository extends JpaRepository<TaskUsersEntity, Long> {
     List<TaskUsersEntity> findByTask(TaskEntity task);
     List<TaskUsersEntity> findByUserDetails(UserEntity userDetails);
-    TaskUsersEntity save(TaskUsersEntity taskUsers);
 }
