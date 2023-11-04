13c4de513411253dedc55a0e38be649c522bdc4c
Mohit Kambli <mohitkambli8@gmail.com>
2023-11-03 22:20:19 -0400

    Removed Getters-Setters and retained Annotations

--- src/main/java/com/OOD/TrelloClone/model/TaskEntity.java
+++ src/main/java/com/OOD/TrelloClone/model/TaskEntity.java
@@ -8,85 +8,11 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.List;
 
-@Getter
-@Setter
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskEntity {
-    public long getTaskID() {
-        return taskID;
-    }
-
-    public void setTaskID(long taskID) {
-        this.taskID = taskID;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTodoTime() {
-        return todoTime;
-    }
-
-    public void setTodoTime(String todoTime) {
-        this.todoTime = todoTime;
-    }
-
-    public String getDoneTime() {
-        return doneTime;
-    }
-
-    public void setDoneTime(String doneTime) {
-        this.doneTime = doneTime;
-    }
-
-    public String getDoingTime() {
-        return doingTime;
-    }
-
-    public void setDoingTime(String doingTime) {
-        this.doingTime = doingTime;
-    }
-
-    public TaskState getState() {
-        return state;
-    }
-
-    public void setState(TaskState state) {
-        this.state = state;
-    }
-
-    public List<UserEntity> getAssignedTo() {
-        return assignedTo;
-    }
-
-    public void setAssignedTo(List<UserEntity> assignedTo) {
-        this.assignedTo = assignedTo;
-    }
-
-    public List<String> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<String> comments) {
-        this.comments = comments;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Exclude
--- src/main/java/com/OOD/TrelloClone/model/UserEntity.java
+++ src/main/java/com/OOD/TrelloClone/model/UserEntity.java
@@ -20,23 +20,7 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long userID;
-
     @NonNull
     private String userName;
 
-    public long getUserID() {
-        return userID;
-    }
-
-    public void setUserID(long userID) {
-        this.userID = userID;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
